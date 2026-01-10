92c5de28087a481da312c8e843d5b3baaf79367e
Vasovski <kateVasovski@gmail.com>
2026-01-10 17:38:39 +0300

    Позеленил тест на подзаголовок с волком 

--- Unit_5_Autotests_and_Test_Driven_Development/Kolobok_test.py
+++ Unit_5_Autotests_and_Test_Driven_Development/Kolobok_test.py
@@ -35,7 +35,7 @@ def test_has_subheader_3_5(text):
 def test_subheaders_order_is_correct(text):
     order = [
         '### 3.1 Заяц',
-        '### 3.2 Собака серая',
+        '### 3.2 Волк',
         '### 3.3 Свин',
         '### 3.4 Медведь',
         '### 3.5 Лиса',
