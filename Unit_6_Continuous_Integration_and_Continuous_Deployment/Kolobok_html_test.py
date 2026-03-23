88896371849591c39237ed3d24c78413b5eb5300
ksenia-ya <ksenia.yakupova@gmail.com>
2026-03-23 18:44:52 +0300

    Написала тест на свинью

--- Unit_6_Continuous_Integration_and_Continuous_Deployment/Kolobok_html_test.py
+++ Unit_6_Continuous_Integration_and_Continuous_Deployment/Kolobok_html_test.py
@@ -23,3 +23,7 @@ def test_has_header(html):
 
 def test_has_subheader1(html):
     assert '<h2>1. Как колобок появился</h2>' in html
+
+
+def test_has_subheader_pig(html):
+    assert '<h3>3.3 Свинья</h3>' in html
