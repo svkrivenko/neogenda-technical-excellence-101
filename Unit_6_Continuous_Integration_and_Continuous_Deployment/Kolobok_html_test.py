4be71c4565c945396cabfa1189545ef618496109
Galina <karpova.galina@hotmail.com>
2026-03-23 22:14:43 +0700

    Тест на Кабана

--- Unit_6_Continuous_Integration_and_Continuous_Deployment/Kolobok_html_test.py
+++ Unit_6_Continuous_Integration_and_Continuous_Deployment/Kolobok_html_test.py
@@ -23,3 +23,6 @@ def test_has_header(html):
 
 def test_has_subheader1(html):
     assert '<h2>1. Как колобок появился</h2>' in html
+
+def test_has_subheader3_pig(html):
+    assert '<h3>3.3 Кабан</h3>' in html
